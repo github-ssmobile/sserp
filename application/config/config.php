779a87f3027213bc2715cf752e2ba57534364333
github-ssmobile <95116334+github-ssmobile@users.noreply.github.com>
2022-02-14 13:13:22 +0530

    config base url change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://117.247.90.162:8800/erp/';
+$ark_root = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
+$ark_root .= "://" . $_SERVER['HTTP_HOST'];
+$ark_root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $ark_root;
 
 $config['image_url'] = 'assets/product_images/';
 $config['erpaccessories_url'] = 'http://192.168.1.144:8088/ssweb/index.php/';
